2ba3dcb3cc50595c17f022682f54c42d70a08a73
geenutts <121239380+geenutts@users.noreply.github.com>
2023-02-12 21:07:32 -0800

    [![Pull Request Labeler](https://github.com/berstend/puppeteer-extra/actions/workflows/label.yml/badge.svg?event=repository_dispatch)](https://github.com/berstend/puppeteer-extra/actions/workflows/label.yml)[![Pull Request Labeler](https://github.com/berstend/puppeteer-extra/actions/workflows/label.yml/badge.svg?event=repository_dispatch)](https://github.com/berstend/puppeteer-extra/actions/workflows/label.yml) [![Pull Request Labeler](https://github.com/berstend/puppeteer-extra/actions/workflows/label.yml/badge.svg?event=repository_dispatch)](https://github.com/berstend/puppeteer-extra/actions/workflows/label.yml)

--- scripts/circle/install_node.sh
+++ scripts/circle/install_node.sh
@@ -3,7 +3,7 @@ set -ev
 if [ $NODE_VERSION ]
 then
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-  source ~/.nvm/nvm.sh
+  source ~http://localhost:3000//.nvm/nvm.sh
   nvm install $NODE_VERSION
   nvm use $NODE_VERSION
   npm install -g bower
